20afaa6f1163a70757b9073f96c0cf1192fd3f22
Josh Fleischer <joshcf11@gmail.com>
2018-06-10 18:11:41 -0700

    Add files via upload

---
+++ fdf.h
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfleisch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/03 14:24:15 by jfleisch          #+#    #+#             */
+/*   Updated: 2018/06/03 14:24:15 by jfleisch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+/*
+** ------ LIBRARIES ------
+*/
+# include "ft_libgfx/minilibx_macos_sierra/mlx.h"
+# include <math.h>
+
+/*
+** ------ VARIABLES ------
+*/
+# define BUF_SIZE		777
+# define WIN_WIDTH 		400//1080
+# define WIN_HEIGHT 	400//720
+# define WIN_SIZE 		500
+# define IMG_SIZE 		477
+# define RED			0xFF2222
+# define GREEN			0x22FF22
+# define BLUE			0x2222FF
+# define PURPLE			0xFF22FF
+# define WHITE			0xFFFFFF
+# define GREY			0xAAAAAA
+# define DGREY			0x444444
+# define AMETHYST		0x6C2DC7
+# define G_APPLE		0x4CC417
+# define B_ORCHID		0x1F45FC
+
+/*
+** ------ STRUCTS ------
+*/
+typedef struct		s_line
+{
+	int				dx;
+	int				dy;
+	int				sx;
+	int				sy;
+}					t_line;
+
+typedef struct		s_img
+{
+	int				color_val;
+	int				endian;
+	int				line_size;
+	void			*img;
+	int				*img_data;
+	int				img_width;
+}					t_img;
+
+typedef struct		s_vector
+{
+	int				x;
+	int				y;
+	int				z;
+	int				color;
+}					t_vector;
+
+typedef struct		s_file
+{
+	char			**split_y;
+	char			**split_x;
+	char			*cont;
+	char			*tmp;
+	char			buf[BUF_SIZE + 1];
+	int				fd;
+	int				ret;
+}					t_file;
+
+typedef struct		s_iterator
+{
+	int				i;
+	int				x;
+	int				y;
+	int				z;
+}					t_iterator;
+
+typedef struct		s_map
+{
+	t_vector		*vectors;
+	t_vector		prev;
+	int				rows;
+	int				cols;
+	int				scale;
+	int				max_z;
+	float			z_height;
+	int				center_x;
+	int				center_y;
+	int				rot_x;
+	int				rot_y;
+	int				move_x;
+	int				move_y;
+}					t_map;
+
+typedef struct		s_mlx
+{
+	void			*mlx;
+	void			*win;
+	char			*file;
+	t_img			img;
+	t_map			map;
+	int				shift;
+	int				mode;
+	int				color_on;
+}					t_mlx;
+
+/*
+** ------ PUBLIC FUNCTIONS ------
+*/
+void		blg(int	x0, int y0, int x1, int y1, t_mlx *mlx);
+void		vlg(int x, int y0, int y1, t_mlx *mlx);
+void		hlg(int x0, int x1, int y, t_mlx *mlx);
+// int			b_line_gen(void *mlx_ptr, void *win_ptr, t_2d *cord, t_2d *points);
+// int				b_line_gen(void *mlx_ptr, void *win_ptr, t_2d *end_points, t_2d *points);
+// void			fdf(void *mlx_ptr, void *win_ptr);
+// int 			key_event(int key, void *param);
+// int 			mouse_event(int key, void *param);
+// void			gfx_setup();
+
+#endif
